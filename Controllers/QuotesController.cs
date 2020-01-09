42fd1cea7ab4183738638f8eb6ba7e43a0a37b7d
matthewshan <matthewshan99@gmail.com>
2020-01-09 01:56:56 -0500

    Init commit

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustardQuotes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuotesController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public QuotesController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
