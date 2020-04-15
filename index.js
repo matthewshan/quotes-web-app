54fd979c415ac533a7216e9930b8a5c135ef9969
Matthew Shan <matthewshan99@gmail.com>
2020-04-14 22:45:59 -0400

    deleted secure

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie : {
-        secure: !process.env.IS_DEV,
+        secure: false,
         maxAge: 1000*60*60*24*7
     }
 }));
