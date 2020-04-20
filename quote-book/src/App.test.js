3957b3f3afb66c268b1d4e441d72b3c2f3a96b22
matthewshan <matthewshan99@gmail.com>
2020-04-19 21:04:44 -0400

    Adjusted Mobile view

--- quote-book/src/App.test.js
+++ quote-book/src/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 import NavBar from './Nav/NavBar.jsx'
+import QuoteCard from './'
+
 
 test('NavBar Email', () => {
   const { getByText } = render(<NavBar user={{email: 'joedoe@gmail.com'}} />);
@@ -9,3 +10,9 @@ test('NavBar Email', () => {
 
   expect(linkElement).toBeInTheDocument();
 });
+
+// test('QuoteCard test', () => {
+//   const { getByText } = render(<QuoteCard quote={"This is my quote"} person={"This person"} />)
+//   getByText("This is my Quote")
+//   getByText("~This person")
+// })
